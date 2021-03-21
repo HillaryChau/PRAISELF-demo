252223985bce456d63d796178dd9c617b729e0dc
Hillary <>
2021-03-21 18:57:05 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const path = require('path')
