35438e9538c35a4954850cade7211d2f75c1f84c
Hillary Chau <hillarychau8@gmail.com>
2021-03-28 22:13:42 -0400

    first commit

--- notes.md
+++ notes.md
@@ -1,1 +1,16 @@
-PRAISELF 
+//PRAISELF//
+
+//homepage//(no need to login to interact with site)
+
+- input drop down (feelings page)
+- drop down, click on item
+- affirmations appear at the bottom
+
+//positive affirmations page//
+- api listing all the feelings and affirmations
+
+//profile page//
+
+//login page//
+
+//sign up page//
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
 const path = require('path')
+
+app.get('/',(req,res) => {
+  res.render("index.ejs");
+});
+
+app.listen(process.env.port || 8000);
+console.log('works')
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + './public'));
